7beb01a71dd2783cdf8a17331c662599dee7652f
RAKESHSHARMA <sharmadevilliers@gmail.com>
2021-11-06 17:09:27 +0530

    Even number

---
+++ Web-Automation/src/com/sgtesting/pageobjrctmodel/Democlass1.java
@@ -0,0 +1,16 @@
+package com.sgtesting.pageobjrctmodel;
+
+public class Democlass1 {
+
+	public static void main(String[] args) 
+		{
+			for(int i=51;i<=71;i++)
+			{
+			if(i%2!=0)
+			{
+			System.out.println(i);
+			}
+			}
+			}
+
+}
